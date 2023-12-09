2717fdb51df03525c9f667c5a782c8eaf30139a3
Yana Burdakova <bu.asti@gmail.com>
2023-12-08 19:29:38 -0500

    feat: add navigate to success page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react';
 import './App.css'
 import StripeCheckout, { Token } from 'react-stripe-checkout';
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const PAYMENT_KEY ="pk_test_51O3JMuB2aDhCaXSC0Toq5EDANjnUk34necZjqXSbdc0QoV0FxTblPkjtrHqesaulQxX7ysPaknOXVSBAlD4UgfCk00lziA2DCG"
 
 function App() {
 
   const [stripeToken, setStripeToken] = useState<Token | null>(null)
-
+  const navigate = useNavigate();
 
   const onToken = (token: Token): void => {
     setStripeToken(token)
@@ -34,6 +35,7 @@ function App() {
             }
           );
           console.log(response.data);
+          navigate('/success')
         } catch (error) {
           console.error("Ошибка при отправке запроса:", error);
         }
@@ -46,18 +48,20 @@ function App() {
   return (
     <>
       <div>
-
-        <StripeCheckout 
-          token={onToken}
-          stripeKey={PAYMENT_KEY}
-          name="Farmer cheese company"
-          image="./assets/react.svg"
-          billingAddress
-          shippingAddress
-          description='Your total is $20'
-          amount={2000}
-          ComponentClass="paymentButton"
-        />
+        {stripeToken 
+        ? <p>Processing... Please wait.</p>
+        : <StripeCheckout 
+        token={onToken}
+        stripeKey={PAYMENT_KEY}
+        name="Farmer cheese company"
+        image="./assets/react.svg"
+        billingAddress
+        shippingAddress
+        description='Your total is $20'
+        amount={2000}
+        ComponentClass="paymentButton"
+      />}
+        
           
       </div>
     </>
