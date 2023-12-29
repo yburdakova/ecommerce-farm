592b2960f829b46258839542fafd3c8ac668d2d3
Yana Burdakova <bu.asti@gmail.com>
2023-12-28 21:53:10 -0500

    feat: implement redux-persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './redux/store.ts'
+import { store, persistor } from './redux/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>
 )
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,15 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./cartRedux";
-import userSlice from "./userRedux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartRedux";
+import userReducer from "./userRedux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({
-  reducer:{
-    cart: cartSlice,
-    user: userSlice,
-  }
-})
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-export type RootState = ReturnType<typeof store.getState>;
-
-export default store
+export const persistor = persistStore(store);
