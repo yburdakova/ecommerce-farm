b2592dcd560b4fc0cfaba253c4a91881d4ed45a1
Yana Burdakova <bu.asti@gmail.com>
2023-12-29 22:24:59 -0500

    fix: fix problem with double order

--- client/src/pages/Success/Success.tsx
+++ client/src/pages/Success/Success.tsx
@@ -1,28 +1,26 @@
-import { useEffect, useState } from "react";
-import { userRequest } from "../../middleware/requestMethods";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import { RootState } from '../../redux/store';
 import { useLocation } from "react-router-dom";
-import { ProductData } from "../../constants/types";
-
+import { RootState } from '../../redux/store';
+import { userRequest } from "../../middleware/requestMethods";
 
 const Success = () => {
   const location = useLocation();
-  
-  const data = location.state.stripeData;
-  const cart = location.state.cart;
+  const data = location.state.stripeData; // Предполагается, что data доступна при монтировании
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
   const [orderId, setOrderId] = useState(null);
-
+  const isOrderCreated = useRef(false); // Флаг для отслеживания создания заказа
 
   useEffect(() => {
     const createOrder = async () => {
-      console.log(currentUser);      
+      console.log(currentUser);
+      const cart = location.state.cart; // Переместил cart внутрь функции, так как она используется только здесь
+
       if (currentUser) {
         console.log(cart.products);
         const order = {
           userId: currentUser._id,
-          products: cart.products.map((item: ProductData) => ({
+          products: cart.products.map((item) => ({
             productId: item._id,
             quantity: item.quantity,
             title: item.title,
@@ -31,29 +29,33 @@ const Success = () => {
           })),
           amount: cart.totalPrice,
           address: data.billing_details.address,
-        }
+        };
         console.log(order);
+
         try {
           const res = await userRequest(currentUser.accessToken).post("/orders", order);
-          console.log (`Data for DB: ${res.data}`)
+          console.log(`Data for DB: ${res.data}`);
           setOrderId(res.data._id);
-        } catch (error){
+        } catch (error) {
           console.log(error);
         }
       }
     };
-    data && createOrder();
-  }, [cart, data, currentUser]);
+    if (data && !isOrderCreated.current) {
+      createOrder();
+      isOrderCreated.current = true; 
+    }
+  }, [data, currentUser]); 
 
   return (
     <div>
       {orderId
         ? `Order has been created successfully. Your order number is ${orderId}`
-        : `Successfull. Your order is being prepared...`}
+        : `Successful. Your order is being prepared...`}
       <button>Go to Homepage</button>
       <button>See all orders</button>
     </div>
-  )
+  );
 }
 
-export default Success
+export default Success;
